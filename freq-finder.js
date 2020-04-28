e84274c3aa4971f6ec17083f36025ea220572451
Charlie Holmes <soundmanforiam@gmail.com>
2020-04-28 15:40:59 -0500

    bootstraped

--- freq-finder.js
+++ freq-finder.js
@@ -1,4 +1,3 @@
-
 // function scrollToCenter()
 // {
 //   let keyWidth = window.screen.width / 10;
@@ -6,20 +5,19 @@
 //   document.getElementById('kbd-wrapper').scrollLeft = c4Pos;
 // }
 
-  audio = new (window.AudioContext || window.webkitAudioContext)()
+audio = new(window.AudioContext || window.webkitAudioContext)()
 
-function playNote(frequency, volume, duration)
-{
-  
-  document.getElementById('freq').innerText = frequency + "hz";
-    var halfPeriod = 1/frequency/2
-    if(duration > halfPeriod) duration -= duration % halfPeriod
+function playNote(frequency, volume, duration) {
+
+    document.getElementById('freq').innerText = frequency + "hz";
+    var halfPeriod = 1 / frequency / 2
+    if (duration > halfPeriod) duration -= duration % halfPeriod
     else duration = halfPeriod
 
     var g = audio.createGain()
     var o = audio.createOscillator()
     o.connect(g)
-    g.connect(audio.destination) 
+    g.connect(audio.destination)
 
     o.frequency.value = frequency
     g.gain.value = volume
@@ -29,24 +27,24 @@ function playNote(frequency, volume, duration)
 
 // media query event handler
 if (matchMedia) {
-  const mq = window.matchMedia("(min-width: 500px)");
-  mq.addListener(WidthChange);
-  WidthChange(mq);
-  }
-  
-  // media query change
-  function WidthChange(mq) {
-  if (mq.matches) {
-  // window width is at least 500px
-  let keyWidth = window.screen.width / 10;
-  let c4Pos = keyWidth * 23
-  document.getElementById('kbd-wrapper').scrollLeft = c4Pos;
-  } else {
-  // window width is less than 500px
-  let keyWidth = window.screen.height / 5.5;
-  let c4Pos = keyWidth * 23
-  document.getElementById('kbd-wrapper').scrollLeft = c4Pos;
+    const mq = window.matchMedia("(min-width: 500px)");
+    mq.addListener(WidthChange);
+    WidthChange(mq);
+}
+
+// media query change
+function WidthChange(mq) {
+    if (mq.matches) {
+        // window width is at least 500px
+        let keyWidth = window.screen.width / 10;
+        let c4Pos = keyWidth * 23
+        document.getElementById('keyboard').scrollLeft = c4Pos;
+    } else {
+        // window width is less than 500px
+        let keyWidth = window.screen.height / 5.5;
+        let c4Pos = keyWidth * 23
+        document.getElementById('keyboard').scrollLeft = c4Pos;
+
+    }
 
 }
-  
-  }
